e562c287039cc3897c0ca9fc9fd8339a4a7941ff
Jac Casey <JacCas2014@users.noreply.github.com>
2015-11-29 19:05:38 +0000

    Jac - complete functionality for word count.

--- word_count_test.clj
+++ word_count_test.clj
@@ -6,18 +6,18 @@
 (fact "count-one-word"
       (word-count "word") => {"word" 1})
 
-(future-fact "count-one-of-each"
+(fact "count-one-of-each"
       (word-count "one of each") => {"one" 1 "of" 1 "each" 1})
 
-(future-fact "count-multiple-occurrences"
+(fact "count-multiple-occurrences"
       (word-count "one fish two fish red fish blue fish") => {"one" 1 "fish" 4 "two" 1 "red" 1 "blue" 1})
 
-(future-fact "ignore-punctuation"
+(fact "ignore-punctuation"
       (word-count "car : carpet as java : javascript!!&@$%^&") => {"car" 1, "carpet" 1 "as" 1 "java" 1 "javascript" 1})
 
-(future-fact "include-numbers"
+(fact "include-numbers"
       (word-count "testing, 1, 2 testing") => {"testing" 2 "1" 1 "2" 1})
 
-(future-fact "normalize-case"
+(fact "normalize-case"
       (word-count "go Go GO") => {"go" 3})
 
--- wordcount.clj
+++ wordcount.clj
@@ -1,4 +1,9 @@
- (ns wordcount)
+ (ns wordcount
+  (:require [clojure.string :as str]))
+
+ (defn remove-punctuation [words]
+  (str/replace words #"\p{Punct}" ""))
 
  (defn word-count [words]
-  {"word" 1})
+  (let [vector-of-words (str/split (remove-punctuation (str/lower-case words)) #"\s+")]
+   (reduce #(assoc %1 %2 (inc (%1 %2 0))) {} vector-of-words)))
