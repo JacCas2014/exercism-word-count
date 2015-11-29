d819fd8a6f967d64bdba66bdb34464c81254d743
Jac Casey <JacCas2014@users.noreply.github.com>
2015-11-29 19:13:51 +0000

    Jac - refactor of word-count function

--- wordcount.clj
+++ wordcount.clj
@@ -5,5 +5,8 @@
   (str/replace words #"\p{Punct}" ""))
 
  (defn word-count [words]
-  (let [vector-of-words (str/split (remove-punctuation (str/lower-case words)) #"\s+")]
+  (let [vector-of-words (-> words
+                            (str/lower-case)
+                            (remove-punctuation)
+                            (str/split #"\s+"))]
    (reduce #(assoc %1 %2 (inc (%1 %2 0))) {} vector-of-words)))
